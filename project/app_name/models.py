1ee5ec928de44fbb71515116bdae6977f691d4ff
Willem Van Onsem <yourfriends@hapytex.eu>
2025-03-08 15:03:45 +0100

    add models as specified in the "QuerySet API reference" section: https://docs.djangoproject.com/en/dev/ref/models/querysets/

--- project/app_name/models.py
+++ project/app_name/models.py
@@ -18,3 +18,32 @@ class Pizza(models.Model):
             self.name,
             ", ".join(topping.name for topping in self.toppings.all()),
         )
+
+class City(models.Model):
+    # ...
+    pass
+
+
+class Person(models.Model):
+    # ...
+    hometown = models.ForeignKey(
+        City,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+
+class Chapter(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=256)
+    chapters = models.ManyToManyField(Chapter)
+    author = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+class Restaurant(models.Model):
+    pizzas = models.ManyToManyField(Pizza, related_name="restaurants")
+    best_pizza = models.ForeignKey(
+        Pizza, related_name="championed_by", on_delete=models.CASCADE
+    )
