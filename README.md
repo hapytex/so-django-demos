4f9145ffa1475bdfd39d940d82198c35c1cd4f90
Willem Van Onsem <yourfriends@hapytex.eu>
2025-10-05 19:32:18 +0200

    add admin for pizza model

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# [Question](https://stackoverflow.com/questions/https://stackoverflow.com/q/79658640/67579/)
+# so-django-demos
 
+Used to demo certain concepts based on questions on [*StackOverflow*](https://stackoverflow.com).
 
-null
-# Answer
+By default it works with an sqlite database with a small amount of sample data.
 
-
+The database also has a user `test` with `test` as password to log in.
--- project/app_name/models.py
+++ project/app_name/models.py
@@ -7,10 +7,13 @@ from django.db import models
 class Topping(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
 
 class Pizza(models.Model):
     name = models.CharField(max_length=50)
-    toppings = models.ManyToManyField(Topping)
+    toppings = models.ManyToManyField(Topping, blank=True)
 
     def __str__(self):
         return "%s (%s)" % (
